36e014afa35c80274196077ce84ce50de78490aa
Tomasz Klim <github@tomaszklim.pl>
2016-01-14 14:04:21 +0100

    simplified random number generation in setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if grep -q /opt/farm/scripts/check/clock.sh /etc/crontab; then
 	echo "removing old crontab entry"
@@ -7,16 +7,5 @@ fi
 
 if ! grep -q /opt/sf-ntp/cron/check.sh /etc/crontab && [ "`ps aux|grep ntpd|grep -v grep`" = "" ]; then
 	echo "setting up crontab entry"
-
-	if [ -x /usr/bin/shuf ]; then
-		rn=`shuf -i0-59 -n1`
-	else
-		rn=$((RANDOM%60))
-	fi
-
-	if [ "$rn" = "" ] || [ "$rn" = "0" ]; then
-		rn=3
-	fi
-
-	echo "$rn 6 * * * root /opt/sf-ntp/cron/check.sh" >>/etc/crontab
+	echo "$((RANDOM%60)) 6 * * * root /opt/sf-ntp/cron/check.sh" >>/etc/crontab
 fi
