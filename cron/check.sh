1992820faad7aa1a022e3382875a4c4719052fa5
Tomasz Klim <github@tomaszklim.pl>
2016-09-05 15:27:47 +0200

    suppress dump warnings from ntpdate

--- cron/check.sh
+++ cron/check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Tomasz Klim, 2009, 2015, 2016
 
-/usr/sbin/ntpdate ntp.task.gda.pl ntp.icm.edu.pl tempus2.gum.gov.pl tempus1.gum.gov.pl >>/var/log/ntpdate
+/usr/sbin/ntpdate ntp.task.gda.pl ntp.icm.edu.pl tempus2.gum.gov.pl tempus1.gum.gov.pl 2>&1 |grep -v "no server suitable for synchronization found" >>/var/log/ntpdate
--- setup.sh
+++ setup.sh
@@ -6,11 +6,6 @@ if [ "$HWTYPE" = "container" ] || [ "$HWTYPE" = "lxc" ]; then
 	exit 0
 fi
 
-if grep -q /opt/farm/scripts/check/clock.sh /etc/crontab; then
-	echo "removing old crontab entry"
-	sed -i -e "/farm\/scripts\/check\/clock.sh/d" /etc/crontab
-fi
-
 if ! grep -q /opt/farm/ext/ntp/cron/check.sh /etc/crontab && [ "`ps aux|grep ntpd|grep -v grep`" = "" ]; then
 	echo "setting up crontab entry"
 	echo "$((RANDOM%60)) 6 * * * root /opt/farm/ext/ntp/cron/check.sh" >>/etc/crontab
